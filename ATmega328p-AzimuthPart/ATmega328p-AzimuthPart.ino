dd886ccc06b60a5b3c337f8ffde35f441c9d2955
YuYanDev <sakura@alleysakura.com>
2022-05-22 19:05:08 +0800

    fix: full defined

--- ATmega328p-AzimuthPart/ATmega328p-AzimuthPart.ino
+++ ATmega328p-AzimuthPart/ATmega328p-AzimuthPart.ino
@@ -11,8 +11,31 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
+/**
+ * @brief important!!!!
+ * For MAX490 Chip
+ * TX - TX
+ * RX - RX
+ */
 #define RS422_TX 2
 #define RS422_RX 3
+
+/**
+ * @brief For Geared Motor Defined
+ * 
+ */
+#define GEARED_MOTOR_DIRECTION_SWITCH 4
+#define GEARED_MOTOR_CW_PWM 5
+#define GEARED_MOTOR_CCW_PWM 6
+#define GEARED_MOTOR_POWER_SWITCH 7
+
+/**
+ * @brief Pulse encoder
+ * 
+ */
+
+#define PULSE_ENCODER_A 8
+#define PULSE_ENCODER_A 9
 
 SoftwareSerial RS422 = SoftwareSerial(RS422_RX, RS422_TX);
 
