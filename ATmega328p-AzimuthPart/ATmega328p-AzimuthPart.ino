d96cb102eb13abe28be0651d118d56e100146751
yuyan <sakura@alleysakura.com>
2022-08-18 20:17:25 +0800

    feat: az

--- ATmega328p-AzimuthPart/ATmega328p-AzimuthPart.ino
+++ ATmega328p-AzimuthPart/ATmega328p-AzimuthPart.ino
@@ -1,48 +1,166 @@
 /**
  * @file ATmega328p-AzimuthPart.ino
  * @author BG5ABL(sakura@alleysakura.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-05-22
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
-#include <SoftwareSerial.h>
 #include <Wire.h>
+// #include <avr/wdt.h>
 
-/**
- * @brief important!!!!
- * For MAX490 Chip
- * TX - TX
- * RX - RX
- */
-#define RS422_TX 2
-#define RS422_RX 3
+#define IS_DEBUG true
 
-/**
- * @brief For Geared Motor Defined
- * 
- */
-#define GEARED_MOTOR_DIRECTION_SWITCH 4
-#define GEARED_MOTOR_CW_PWM 5
-#define GEARED_MOTOR_CCW_PWM 6
-#define GEARED_MOTOR_POWER_SWITCH 7
+#define GEARED_MOTOR_POWER_SWITCH 2
+#define GEARED_MOTOR_POWER_CW 5
+#define GEARED_MOTOR_POWER_CCW 6
 
-/**
- * @brief Pulse encoder
- * 
- */
+int ADXL345 = 0x53;
 
-#define PULSE_ENCODER_A 8
-#define PULSE_ENCODER_B 9
+float ADXL345XOut, ADXL345YOut, ADXL345ZOut, ADXL345ZAngle;
 
-SoftwareSerial RS422 = SoftwareSerial(RS422_RX, RS422_TX);
+void startRotateCW(String serialString) {
+    digitalWrite(GEARED_MOTOR_POWER_SWITCH, 0);
+    digitalWrite(GEARED_MOTOR_POWER_CCW, 0);
+    digitalWrite(GEARED_MOTOR_POWER_CW, 1);
+    digitalWrite(GEARED_MOTOR_POWER_SWITCH, 1);
+    Serial.println("+CW:OK");
+}
+
+void startRotateCCW(String serialString) {
+    digitalWrite(GEARED_MOTOR_POWER_SWITCH, 0);
+    digitalWrite(GEARED_MOTOR_POWER_CW, 0);
+    digitalWrite(GEARED_MOTOR_POWER_CCW, 1);
+    digitalWrite(GEARED_MOTOR_POWER_SWITCH, 1);
+    Serial.println("+CCW:OK");
+}
+
+void stopRotate() {
+    digitalWrite(GEARED_MOTOR_POWER_SWITCH, 0);
+    digitalWrite(GEARED_MOTOR_POWER_CW, 0);
+    digitalWrite(GEARED_MOTOR_POWER_CCW, 0);
+    Serial.println("+STOP:OK");
+}
+
+void resetRotate() {}
+
+void readADXL345() {
+    Wire.beginTransmission(ADXL345);
+    Wire.write(0x32);
+    Wire.endTransmission(false);
+    Wire.requestFrom(ADXL345, 6, true);
+
+    ADXL345XOut = (Wire.read() | Wire.read() << 8);
+    ADXL345XOut = ADXL345XOut / 256;
+    ADXL345YOut = (Wire.read() | Wire.read() << 8);
+    ADXL345YOut = ADXL345YOut / 256;
+    ADXL345ZOut = (Wire.read() | Wire.read() << 8);
+    ADXL345ZOut = ADXL345ZOut / 256;
+
+    if (ADXL345XOut > 0) {
+        ADXL345ZAngle = ADXL345YOut * 90;
+    } else {
+        ADXL345ZAngle = 180 - ADXL345YOut * 90;
+    }
+}
+
+void printADXL345() {
+    Serial.print("+ANGLE:");
+    Serial.print(ADXL345XOut);
+    Serial.print(",");
+    Serial.print(ADXL345YOut);
+    Serial.print(",");
+    Serial.print(ADXL345ZOut);
+    Serial.print(",");
+    Serial.println(ADXL345ZAngle);
+}
+
+void onSerialCall() {
+    String serialString;
+
+    // 当串口有数据则循环运行如下处理
+    while (Serial.available()) {
+        serialString = serialString + (char)Serial.read();
+    }
+
+    if (IS_DEBUG) {
+        Serial.print("GetSerial");
+        Serial.println(serialString);
+    }
+
+    if (serialString == "AT+HAND") {
+        Serial.print("+HAND:OK");
+        // wdt_enable(WDTO_2S);
+        return;
+    }
+
+    if (serialString == "AT+BYE") {
+        Serial.print("+BYE:OK");
+        // wdt_disable();
+    }
+
+    if (serialString.indexOf("AT+CCW") > -1) {
+        startRotateCCW(serialString);
+        // wdt_reset();
+        return;
+    }
+
+    if (serialString.indexOf("AT+CW") > -1) {
+        startRotateCW(serialString);
+        // wdt_reset();
+        return;
+    }
+
+    if (serialString == "AT+STOP") {
+        stopRotate();
+        // wdt_reset();
+        return;
+    }
+
+    if (serialString == "AT+RESET") {
+        resetRotate();
+        // wdt_reset();
+        return;
+    }
+
+    if (serialString == "AT+ANGLE?") {
+        readADXL345();
+        printADXL345();
+        // wdt_reset();
+        return;
+    }
+}
+
+void initPIN() {
+    pinMode(GEARED_MOTOR_POWER_SWITCH, OUTPUT);
+    digitalWrite(GEARED_MOTOR_POWER_SWITCH, 0);
+    pinMode(GEARED_MOTOR_POWER_CW, OUTPUT);
+    digitalWrite(GEARED_MOTOR_POWER_CW, 0);
+    pinMode(GEARED_MOTOR_POWER_CCW, OUTPUT);
+    digitalWrite(GEARED_MOTOR_POWER_CCW, 0);
+}
+
+void initADXL345() {
+    Wire.begin();
+    Wire.beginTransmission(ADXL345);
+    Wire.write(0x2D);
+    Wire.write(8);
+    Wire.endTransmission();
+}
 
 void setup() {
     Serial.begin(9600);
-    RS422.begin(9600);
+    initPIN();
+    initADXL345();
+    Serial.println("+SYS:OK");
+    delay(100);
 }
 
 void loop() {
+    if (Serial.available()) {
+        onSerialCall();
+    }
+    delay(100);
 }
